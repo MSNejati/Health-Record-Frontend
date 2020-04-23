c47b5108809608fef43fce23141d7e0dfbb9aeae
Mohammad Sajjad Nejati <nejati.mohammadsajjad@gmail.com>
2020-04-23 23:19:26 +0500

    Create test page for profile

---
+++ src/components/patient/PatientProfile.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class PatientProfile extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Patient profile</h1>
+      </div>
+    );
+  }
+}
