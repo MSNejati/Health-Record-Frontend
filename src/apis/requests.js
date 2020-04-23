a63eb3db7e7a0f0da799917016d9bb0a50a1fabe
Mohammad Sajjad Nejati <nejati.mohammadsajjad@gmail.com>
2020-04-23 11:58:44 +0500

    Setting up a function for user API

--- src/apis/requests.js
+++ src/apis/requests.js
@@ -1,12 +1,12 @@
-const HOSTNAME = "https://localhost:8000/";
+const HOSTNAME = "http://localhost:8000/";
 
-const apiRequest = (type, id = false) => {
+export const userAPI = (type) => {
   switch (type) {
     case "LOGIN":
       return `${HOSTNAME}auth/login/`;
     case "LOGOUT":
       return `${HOSTNAME}auth/logout/`;
-    case "LOAD":
+    case "RETRIEVE":
       return `${HOSTNAME}auth/retrieve/`;
     case "REFRESH":
       return `${HOSTNAME}auth/refresh/`;
