46c7e8cf0510c7637e6cbaff020667b6a862b4c4
Alireza Azizi <alirezaazizi1396622@gmail.com>
2020-06-02 00:17:46 +0430

    Fix edit and remove functionality

--- src/components/doctor/AppointmentManage.js
+++ src/components/doctor/AppointmentManage.js
@@ -8,6 +8,49 @@ import { doctorAPI } from "./../../apis/requests";
 import axios from "axios";
 import Table from "./layout/Table";
 import Item from "./layout/Item";
+import Swal from "sweetalert2";
+
+function Row(props) {
+  return (
+    <div
+      className="mt-4 text-right"
+      style={{
+        marginRight: "7%",
+        marginLeft: "7%",
+      }}
+    >
+      <h3>{props.header}</h3>
+      <div className="d-flex">
+        <div
+          className="border ml-2 shadow-sm"
+          style={{
+            height: "270px",
+            width: "50%",
+            overflowX: "auto",
+            overflowY: "auto",
+          }}
+        >
+          <Table url={props.url} add={props.addItem} name={props.name} />
+        </div>
+        <div
+          className="border mr-2 p-2 shadow-sm"
+          style={{
+            height: "270px",
+            width: "50%",
+          }}
+        >
+          {props.items.map((item, index) => (
+            <Item
+              item={item}
+              key={index}
+              onDelete={() => props.deleteItem(item.id, props.name)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export class AppointmentManage extends Component {
   // static propTypes = {
@@ -66,11 +109,19 @@ export class AppointmentManage extends Component {
     this.state.medicines.forEach((x) => body["medicines"].push(x.id));
     axios
       .patch(doctorAPI("APPOINTMENTS", this.props.match.params.id), body, {})
-      .then((res) => console.log("00"));
+      .then((res) =>
+        Swal.fire({
+          icon: "success",
+          title: "تغییرات با موفقیت ذخیره شد.",
+          showConfirmButton: false,
+          timer: 2000,
+        })
+      );
   };
+
   render() {
     return this.state.load ? (
-      <div className="d-flex">
+      <div className="d-flex bg-light">
         <SideBar />
         <div id="content" style={{ width: "100%" }}>
           <div className="">
@@ -126,173 +177,50 @@ export class AppointmentManage extends Component {
               </div>
             </div>
             {/* ******************************** */}
-            <div
-              className="mt-4 text-right"
-              style={{
-                marginRight: "7%",
-                marginLeft: "7%",
-              }}
-            >
-              <h3>توصیه ها</h3>
-              <div className="d-flex">
-                <div
-                  className="border ml-2 shadow-sm"
-                  style={{
-                    height: "250px",
-                    width: "100%",
-                  }}
-                >
-                  <Table url="ADVICES" add={this.addItem} name="advices" />
-                </div>
-                <div
-                  className="border mr-2 p-2 shadow-sm"
-                  style={{
-                    height: "250px",
-                    width: "100%",
-                  }}
-                >
-                  {this.state.advices.map((item, index) => (
-                    <Item
-                      item={item}
-                      key={index}
-                      onDelete={() => this.deleteItem(item.id, "advices")}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
+            <Row
+              header="توصیه ها"
+              name="advices"
+              url="ADVICES"
+              items={this.state.advices}
+              deleteItem={this.deleteItem}
+              addItem={this.addItem}
+            />
             {/* ******************************** */}
-            <div
-              className="mt-4 text-right"
-              style={{
-                marginRight: "7%",
-                marginLeft: "7%",
-              }}
-            >
-              <h3>بیماری ها</h3>
-              <div className="d-flex">
-                <div
-                  className="border ml-2 shadow-sm"
-                  style={{
-                    height: "250px",
-                    width: "100%",
-                  }}
-                >
-                  <Table url="DISEASES" add={this.addItem} name="diseases" />
-                </div>
-                <div
-                  className="border mr-2 p-2 shadow-sm"
-                  style={{
-                    height: "250px",
-                    width: "100%",
-                  }}
-                >
-                  {this.state.diseases.map((item, index) => (
-                    <Item
-                      item={item}
-                      key={index}
-                      onDelete={() => this.deleteItem(item.id, "diseases")}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
+            <Row
+              header="بیماری ها"
+              name="diseases"
+              url="DISEASES"
+              items={this.state.diseases}
+              deleteItem={this.deleteItem}
+              addItem={this.addItem}
+            />
             {/* ************************************ */}
-            <div
-              className="mt-4 text-right"
-              style={{
-                marginRight: "7%",
-                marginLeft: "7%",
-              }}
-            >
-              <h3>علامت ها</h3>
-              <div className="d-flex">
-                <div
-                  className="border ml-2 shadow-sm"
-                  style={{
-                    height: "250px",
-                    width: "100%",
-                  }}
-                >
-                  <Table url="SYMPTOMS" add={this.addItem} name="symptoms" />
-                </div>
-                <div
-                  className="border mr-2 p-2 shadow-sm"
-                  style={{
-                    height: "250px",
-                    width: "100%",
-                  }}
-                >
-                  {this.state.symptoms.map((item, index) => (
-                    <Item
-                      item={item}
-                      key={index}
-                      onDelete={() => this.deleteItem(item.id, "symptoms")}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
+            <Row
+              header="علامت ها"
+              name="symptoms"
+              url="SYMPTOMS"
+              items={this.state.symptoms}
+              deleteItem={this.deleteItem}
+              addItem={this.addItem}
+            />
             {/* ******************************** */}
-            <div
-              className="mt-4 text-right"
-              style={{
-                marginRight: "7%",
-                marginLeft: "7%",
-              }}
-            >
-              <h3>داروها</h3>
-              <div className="d-flex">
-                <div
-                  className="border ml-2 shadow-sm"
-                  style={{
-                    height: "250px",
-                    width: "100%",
-                  }}
-                >
-                  <Table url="MEDICINES" add={this.addItem} name="medicines" />
-                </div>
-                <div
-                  className="border mr-2 p-2 shadow-sm"
-                  style={{
-                    height: "250px",
-                    width: "100%",
-                  }}
-                >
-                  {this.state.medicines.map((item, index) => (
-                    <Item
-                      item={item}
-                      key={index}
-                      onDelete={() => this.deleteItem(item.id, "medicines")}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
+            <Row
+              header="دارو ها"
+              name="medicines"
+              url="MEDICINES"
+              items={this.state.medicines}
+              deleteItem={this.deleteItem}
+              addItem={this.addItem}
+            />
             {/* ******************************** */}
-            <div
-              className="mt-4 text-right"
-              style={{
-                marginRight: "7%",
-                marginLeft: "7%",
-              }}
-            >
-              <div
-                className="text-left"
-                style={{
-                  width: "100%",
-                }}
+            <div className="text-center">
+              <button
+                className="btn btn-lg btn-primary m-4"
+                onClick={this.onSubmit}
               >
-                <button
-                  className="btn btn-primary btn-lg"
-                  onClick={this.onSubmit}
-                >
-                  ذخیره تغییرات
-                </button>
-              </div>
+                ثبت تغییرات
+              </button>
             </div>
-            <br />
-            <br />
           </div>
         </div>
       </div>
