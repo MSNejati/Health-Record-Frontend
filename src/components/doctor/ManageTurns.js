747a8da205ab11fd7b5c851fd5bf927fe4fef274
Alireza Azizi <alirezaazizi1396622@gmail.com>
2020-06-06 15:35:54 +0430

    Put add turn form in dialog and filter by delta day

--- src/components/doctor/ManageTurns.js
+++ src/components/doctor/ManageTurns.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
-import { doctorAPI } from "./../../apis/requests";
+import { doctorAPI, patientAPI } from "./../../apis/requests";
 import AddTurnForm from "./AddTurnForm";
 import Loading from "./../layout/Loading";
 import SideBar from "./../layout/SideBar";
@@ -16,27 +16,18 @@ export class ManageTurns extends Component {
     next: null,
     prev: null,
     current: null,
+    params: { start: "", end: "", delta_day: "" },
   };
 
   componentDidMount() {
-    axios
-      .get(
-        doctorAPI("CALENDARS"),
-        {
-          params: {
-            ordering: "day,start_time",
-          },
-        },
-        {}
-      )
-      .then((res) => {
-        this.setState({
-          turns: res.data.results,
-          next: res.data.next,
-          prev: res.data.previous,
-          current: doctorAPI("CALENDARS"),
-        });
+    axios.get(doctorAPI("CALENDARS"), {}, {}).then((res) => {
+      this.setState({
+        turns: res.data.results,
+        next: res.data.next,
+        prev: res.data.previous,
+        current: doctorAPI("CALENDARS"),
       });
+    });
   }
 
   parser = (time) => {
@@ -68,21 +59,52 @@ export class ManageTurns extends Component {
   };
 
   getPage = () => {
-    axios.get(this.state.current, {}, {}).then((res) => {
-      this.setState({
-        turns: res.data.results,
-        next: res.data.next,
-        prev: res.data.previous,
+    axios
+      .get(
+        this.state.current,
+        {
+          params: this.state.params,
+        },
+        {}
+      )
+      .then((res) => {
+        this.setState({
+          turns: res.data.results,
+          next: res.data.next,
+          prev: res.data.previous,
+        });
+        // Swal.fire({
+        //   icon: "success",
+        //   title: "نوبت با موفقیت افزوده شد",
+        //   showConfirmButton: false,
+        //   timer: 2000,
+        // });
       });
-      Swal.fire({
-        icon: "success",
-        title: "نوبت با موفقیت افزوده شد",
-        showConfirmButton: false,
-        timer: 2000,
-      });
-    });
   };
 
+  onParamsChange = (e) => {
+    var params = this.state.params;
+    params[e.target.name] = e.target.value;
+    this.setState({ params: params });
+  };
+  submitFilter = () => {
+    axios
+      .get(
+        doctorAPI("CALENDARS"),
+        {
+          params: this.state.params,
+        },
+        {}
+      )
+      .then((res) => {
+        this.setState({
+          turns: res.data.results,
+          next: res.data.next,
+          prev: res.data.previous,
+          current: doctorAPI("CALENDARS"),
+        });
+      });
+  };
   render() {
     return this.state.turns ? (
       <div
@@ -102,22 +124,51 @@ export class ManageTurns extends Component {
               }
             >
               <h5 className="card-header text-body text-center pt-3 font-weight-bold">
-                افزودن نوبت
-              </h5>
-              <div className="card-body">
-                <AddTurnForm onSubmit={this.getPage} />
-              </div>
-            </div>
-            <div
-              className={
-                this.props.isActive
-                  ? "my-card card text-right active"
-                  : "my-card card text-right"
-              }
-            >
-              <h5 className="card-header text-body text-center pt-3 font-weight-bold">
                 لیست نوبت های ثبت شده
               </h5>
+              <div
+                className="d-sm-inline-flex p-2 justify-content-center"
+                style={{ whiteSpace: "nowrap" }}
+              >
+                <input
+                  placeholder="چند روز آینده"
+                  type="number"
+                  style={{ maxWidth: "160px" }}
+                  className="rounded border"
+                  value={this.state.params.delta_day}
+                  onChange={this.onParamsChange}
+                  name="delta_day"
+                />
+                <input
+                  placeholder="حد پایین زمانی"
+                  onFocus={(e) => (e.target.type = "date")}
+                  onBlur={(e) => (e.target.type = "text")}
+                  type="text"
+                  style={{ maxWidth: "160px" }}
+                  className="rounded border"
+                  value={this.state.params.start}
+                  onChange={this.onParamsChange}
+                  name="start"
+                />
+                <input
+                  placeholder="حد بالا زمانی"
+                  onFocus={(e) => (e.target.type = "date")}
+                  onBlur={(e) => (e.target.type = "text")}
+                  type="text"
+                  style={{ maxWidth: "160px" }}
+                  className="rounded border"
+                  value={this.state.params.end}
+                  onChange={this.onParamsChange}
+                  name="end"
+                />
+                <button
+                  className="btn btn-sm purple-btn m-1"
+                  onClick={this.submitFilter}
+                >
+                  اعمال فیلتر
+                </button>
+                {/* <button className="btn btn-sm btn-dark m-1">تایم لاین</button> */}
+              </div>
               <div className="card-body scrollable">
                 <div className="form-row col-md">
                   <table className="table table-striped text-center">
@@ -128,6 +179,7 @@ export class ManageTurns extends Component {
                         <th scope="col">ساعت شروع</th>
                         <th scope="col">تعداد نوبت تعریف شده</th>
                         <th scope="col">تعداد نوبت باقیمانده</th>
+                        <th scope="col"></th>
                         <th scope="col"></th>
                       </tr>
                     </thead>
@@ -143,6 +195,18 @@ export class ManageTurns extends Component {
                           </td>
                           <td style={{ direction: "ltr" }}>{turn.total}</td>
                           <td style={{ direction: "ltr" }}>{turn.remained}</td>
+                          <td>
+                            <button
+                              className="btn btn-info btn-sm"
+                              onClick={() => {
+                                this.props.history.push(
+                                  "/doctor/app?calendar=" + turn.id
+                                );
+                              }}
+                            >
+                              قرارها
+                            </button>
+                          </td>
                           <td>
                             <button
                               className="btn exit-button btn-sm"
@@ -200,6 +264,29 @@ export class ManageTurns extends Component {
                   </nav>
                 </div>
               </div>
+              <div>
+                <button
+                  type="button"
+                  className="btn btn-primary btn-lg m-3 float-left"
+                  data-toggle="modal"
+                  data-target="#modal"
+                >
+                  افزودن تقویم
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="modal fade"
+          id="modal"
+          tabIndex="-1"
+          role="dialog"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-md" role="document">
+            <div className="modal-content p-3">
+              <AddTurnForm onSubmit={this.getPage} />
             </div>
           </div>
         </div>
